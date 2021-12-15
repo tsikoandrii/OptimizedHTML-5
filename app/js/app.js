eefdab3a80d1179872cee42ab00f1eae117d4c3c
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-12-15 21:42:12 +0300

    Rebase js, add js methods

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,1 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
-})
+document.addEventListener('DOMContentLoaded', () => {})
